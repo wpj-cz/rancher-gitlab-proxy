0ce6e6ebda48d4725bb94ed9ebd9c58fe58af56f
Joe <cuchac@email.cz>
2021-03-29 17:38:03 +0200

    Update gitlab.rb

--- gitlab.rb
+++ gitlab.rb
@@ -3,23 +3,23 @@
 nginx['custom_gitlab_server_config'] = "
         # CONNECTION TO rancher-gitlab-proxy BEGIN
         location /login/oauth/authorize {
-                proxy_pass http://127.0.0.1:8888;
+                proxy_pass http://auth-proxy:8888;
         }
 
         location /login/oauth/access_token {
-                proxy_pass http://127.0.0.1:8888;
+                proxy_pass http://auth-proxy:8888;
         }
 
         location /api/v3/user {
-                proxy_pass http://127.0.0.1:8888;
+                proxy_pass http://auth-proxy:8888;
         }
 
         location /api/v3/teams/ {
-                proxy_pass http://127.0.0.1:8888;
+                proxy_pass http://auth-proxy:8888;
         }
 
         location /api/v3/search/users {
-                proxy_pass http://127.0.0.1:8888;
+                proxy_pass http://auth-proxy:8888;
         }
         # CONNECTION TO rancher-gitlab-proxy END
 "

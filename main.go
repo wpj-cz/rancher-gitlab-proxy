8c268bc6f7fd2eee4f5cbc240805df15debe195a
Joe <cuchac@email.cz>
2022-06-22 21:49:46 +0200

    Fix

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func oauthAuthorize(w http.ResponseWriter, req *http.Request, ps httprouter.Para
 	client_id := req.URL.Query().Get("client_id");
 	rancher_urls[client_id] = redirect_uri;
 
-	fmt.Println("redirect_uri", redirect_uri, "client_id", client_id, string(rancher_urls));
+	fmt.Println("redirect_uri", redirect_uri, "client_id", client_id, rancher_urls);
 
 	v := req.URL.Query()
 	v.Add("response_type", "code")
@@ -73,7 +73,7 @@ func oauthAccessToken(w http.ResponseWriter, req *http.Request, ps httprouter.Pa
 		v.Add("redirect_uri", rancher_url + "/verify-auth")
 	}
 
-	fmt.Println("found", found, "client_id", client_id, string(rancher_urls));
+	fmt.Println("found", found, "client_id", client_id, rancher_urls);
 	
 	requestDump, err := httputil.DumpRequest(req, true)
 	if err != nil {

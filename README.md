f87f36082df27a143b125f14ab545c4bff441d3d
Joe <joe@wpj.cz>
2022-10-13 11:59:01 +0200

    Update docs

--- README.md
+++ README.md
@@ -3,11 +3,13 @@ Authenticate Rancher users from Gitlab instance. Uses proxy simulating GitHub AP
 
 All credits goes to https://sandstorm.de/de/blog/post/making-rancher-2-and-gitlab-oauth-authentication-work-together.html
 
-It is now not required to fill `RANCHER_URL` env variable. Rancher url is autodetected from requests now. You can this way use same gitlab instance for several Rancher instances! :-)
+It is now not required to fill `RANCHER_URL` env variable. Rancher url is auto-detected from requests now. You can this way use same gitlab instance for several Rancher instances! :-)
+
+Tested with Gitlab 15 and Rancher 2.6
 
 # Installation using docker
 
-1. Run `wpjsro/rancher-gitlab-proxy:latest` Docker image next to Gitlab. Set `GITLAB_URL` and `RANCHER_URL` env variables, see example `docker-compose.yaml`. 
+1. Run `wpjsro/rancher-gitlab-proxy:latest` Docker image next to Gitlab. Set `GITLAB_URL`, see example `docker-compose.yaml`. 
 1. Configure Gitlab - add config option from `gitlab.rb`
 1. Create Gitlab application with `read_api` scope and `https://<RANCHER_URL>/verify-auth` redirect URL
 1. Configure Rancher to use GitHub authentication, set enterprise host to your Gitlab url, fill in Application ID and Secret from Gitlab.
--- gitlab.rb
+++ gitlab.rb
@@ -1,25 +1,28 @@
 # in GitLab config - gitlab.rb
 
 nginx['custom_gitlab_server_config'] = "
-        # CONNECTION TO rancher-gitlab-proxy BEGIN
-        location /login/oauth/authorize {
-                proxy_pass http://auth-proxy:8888;
-        }
-
-        location /login/oauth/access_token {
-                proxy_pass http://auth-proxy:8888;
-        }
-
-        location /api/v3/user {
-                proxy_pass http://auth-proxy:8888;
-        }
-
-        location /api/v3/teams/ {
-                proxy_pass http://auth-proxy:8888;
-        }
-
-        location /api/v3/search/users {
-                proxy_pass http://auth-proxy:8888;
-        }
-        # CONNECTION TO rancher-gitlab-proxy END
+   # CONNECTION TO rancher-gitlab-proxy BEGIN
+      location = /login/oauth/authorize {
+         proxy_pass http://auth-proxy:8888;
+      }
+      location = /login/oauth/access_token {
+         proxy_pass http://auth-proxy:8888;
+      }
+      location = /api/v3/user {
+         proxy_pass http://auth-proxy:8888;
+      }
+      location = /api/v3/user/orgs {
+         proxy_pass http://auth-proxy:8888;
+      }
+      location = /api/v3/user/teams {
+         proxy_pass http://auth-proxy:8888;
+      }
+      location = /api/v3/teams/ {
+         proxy_pass http://auth-proxy:8888;
+      }
+      location = /api/v3/search/users {
+         proxy_pass http://auth-proxy:8888;
+      }
+   # CONNECTION TO rancher-gitlab-proxy END
+        "
 "
